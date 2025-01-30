91c30fd95386cd5f6b83e0658f7ae2c98c1daa1f
MSTLAUR <mstlaur@gmail.com>
2025-01-30 16:40:37 -0500

    Proc

--- cloudschool/settings.py
+++ cloudschool/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 import environ
 import os
+from django.core.wsgi import get_wsgi_application
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
