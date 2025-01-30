2f8effd0115ec9eb1290d2a48ff3309a0b275cca
MSTLAUR <mstlaur@gmail.com>
2025-01-30 17:21:04 -0500

    all

--- cloudschool/settings.py
+++ cloudschool/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['pianocloudschool-production.up.railway.app', '127.0.0.1', 'localhost','https://pianocloudschool-production.up.railway.app/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
