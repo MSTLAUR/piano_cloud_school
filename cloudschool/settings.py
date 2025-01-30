77e55229082cf16753fae9078ca5488aa5fb2a4e
MSTLAUR <mstlaur@gmail.com>
2025-01-30 17:43:12 -0500

    lite

--- cloudschool/settings.py
+++ cloudschool/settings.py
@@ -91,24 +91,14 @@ WSGI_APPLICATION = 'cloudschool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT', '5432'),
-        }
-    }
+
 
 
 # Password validation
