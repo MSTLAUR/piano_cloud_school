0c60ec618ee88a52653f6556783c78462efbd14f
MSTLAUR <mstlaur@gmail.com>
2025-01-30 17:16:42 -0500

    host

--- cloudschool/settings.py
+++ cloudschool/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['pianocloudschool-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['pianocloudschool-production.up.railway.app', '127.0.0.1', 'localhost','https://pianocloudschool-production.up.railway.app/']
 
 
 # Application definition
@@ -87,12 +87,24 @@ WSGI_APPLICATION = 'cloudschool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
+    }
 
 
 # Password validation
