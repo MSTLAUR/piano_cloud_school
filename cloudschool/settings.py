55cd4f19f1b3e1402147251dafb665379f9e2aa7
MSTLAUR <mstlaur@gmail.com>
2025-02-01 10:22:12 -0500

    back_to

--- cloudschool/settings.py
+++ cloudschool/settings.py
@@ -33,7 +33,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = "True"
 
 ALLOWED_HOSTS = [
   'localhost',
